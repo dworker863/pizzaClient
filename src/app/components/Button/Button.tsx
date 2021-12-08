75d8d623f4905633701dd8518c2ebe26c138662e
dworker <dworker863@gmail.com>
2021-12-08 22:37:43 +0600

    Added scale change effect on hover button

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -11,15 +11,33 @@ interface ButtonProps {
 
 const StyledButton = styled.button`
   ${tw`
+  relative
   py-3
   px-20
   rounded-3xl
   font-bold
   text-white
   bg-green-600
-  hover:px-24
-  transition-all
   `}
+
+  &:before {
+    z-index: -1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    content: '';
+    width: 337px;
+    height: 48px;
+    border-radius: 24px;
+    background-color: rgba(5, 150, 105);
+    transition: all 0.2s;
+  }
+
+  &:hover {
+    &:before {
+      transform: scale(1.083333333333333);
+    }
+  }
 `;
 
 const Button: FC<ButtonProps> = ({ text, location }) => {
